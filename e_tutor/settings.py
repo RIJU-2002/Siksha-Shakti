7b56e63ddfc74482a5a762f7910e20aff5a4d764
RIJU-2002 <104644844+RIJU-2002@users.noreply.github.com>
2023-08-28 13:55:43 +0530

    Update settings.py

--- e_tutor/settings.py
+++ e_tutor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%_kkg7-jq1ko&432)%=yote%g1m%-x^1s-qml_prvjoxd3d+@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL='authentication.User'
 # Application definition
